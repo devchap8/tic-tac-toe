ef2efed70344eb5035dbdd5fcf6a04749284d686
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-02-15 07:55:32 -0500

    Finish GameParameters module

--- gameParameters.js
+++ gameParameters.js
@@ -1,5 +1,34 @@
-function GameParameters() {
-    // console.log("game parameters");
-}
+const GameParameters = (function() {
+    let turn = 0;
+    let ties = 0;
+    let players = [];
+    const getTurn = () => turn;
+    const getTies = () => ties;
+    class Player {
+        constructor(name, piece) {
+            this.name = name;
+            this.piece = piece;
+            this.wins = 0;
+        }
+    }
+    function makePlayers(player1name, player1piece, player2name, player2piece) {
+        let player1 = new Player(player1name, player1piece);
+        let player2 = new Player(player2name, player2piece);
+        players = [player1, player2];
+    }
+    const getPlayers = () => [...players];
+    function clearGameParameters() {
+        turn = 0;
+        ties = 0;
+        players = [];
+    }
+    function addPlayerWin(playerIndex) {
+        players[playerIndex].wins++;
+    }
+    function addTie() {
+        ties++;
+    }
+    return {getTurn, getTies, getPlayers, makePlayers, clearGameParameters, addPlayerWin, addTie};
+})();
 
 export {GameParameters};
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-import {Gameboard} from "./gameboard.js";
+import { Gameboard } from "./gameboard.js";
 import { GameDisplayController } from "./gameDisplayContoller.js";
 import { GameParameters } from "./gameParameters.js";
 import { GameStateController } from "./gameStateController.js";
+
+GameParameters.makePlayers("p1", "X", "p2", "O");
+
