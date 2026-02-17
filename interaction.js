e0a66999aa84e935bc34a8bd508728806ae936e8
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-02-17 12:39:57 -0500

    Finish select menu

--- interaction.js
+++ interaction.js
@@ -9,7 +9,12 @@ const Interaction = (function() {
     const menuReturnButton = document.querySelector(".menuReturnButton");
     const refreshButton = document.querySelector(".refresh");
     const exitButton = document.querySelector(".exit");
+    const selectScreen = document.querySelector(".selectScreen");
+    let catDivs = Array.from(document.querySelectorAll(".catDiv"));
+    let dogDivs = Array.from(document.querySelectorAll(".dogDiv"));
+    const readyButton = document.querySelector(".ready");
 
+    // Game Grid functionality
     const addGameGridEventListener = () => gameGrid.addEventListener("click", playerClicksGrid);
     const removeGameGridEventListener = () => gameGrid.removeEventListener("click", playerClicksGrid);
     function playerClicksGrid(event) {
@@ -24,19 +29,18 @@ const Interaction = (function() {
             GameDisplayController.displayPiece(currentPlayer, currentPos);
             if(GameStateController.checkWon(currentPlayer)) {
                 GameParameters.addPlayerWin(currentPlayer);
-                removeGameGridEventListener(); // Put these into 1 function later
-                removeRefreshButtonEventListener();
+                removeGameEndEventListeners();
                 GameDisplayController.displayGameWinner(currentPlayer);
             }
             else if(turn >=  8) {
                 GameParameters.addTie();
-                removeGameGridEventListener(); // Put these into 1 function later
-                removeRefreshButtonEventListener();
+                removeGameEndEventListeners();
                 GameDisplayController.displayGameTied();
             }
         }
     }
 
+    // Buttons at top of grid / during game end screen
     const addRefreshButtonEventListener = () => refreshButton.addEventListener("click", newRound);
     const removeRefreshButtonEventListener = () => refreshButton.removeEventListener("click", newRound);
     function newRound() {
@@ -44,21 +48,104 @@ const Interaction = (function() {
         Gameboard.clearBoard();
         GameDisplayController.clearGrid();
         GameDisplayController.displayWinsTies();
-        addGameGridEventListener(); // Put these into 1 function later
-        addRefreshButtonEventListener();
+        addGameEndEventListeners();
     }
     const addNewGameButtonEventListener = () => newGameButton.addEventListener("click", function () {
         GameDisplayController.toggleGameEndScreen();
         newRound();
     })
 
-    function addEventListeners() {
+    const addExitButtonEventListener = () => exitButton.addEventListener("click", returnToMenu);
+    const removeExitButtonEventListener = () => exitButton.removeEventListener("click", returnToMenu);
+    function returnToMenu() {
+        GameParameters.clearGameParameters();
+        GameDisplayController.toggleGameScreen();
+        GameDisplayController.toggleSelectScreen();
+    }
+    const addMenuReturnButtonEventListener = () => menuReturnButton.addEventListener("click", function () {
+        GameDisplayController.toggleGameEndScreen();
+        returnToMenu();
+    })
+
+    // Character Select Menu Functionality
+    const addSelectScreenEventListener = () => selectScreen.addEventListener("click", selectPlayer);
+    function selectPlayer(event) {
+        if(event.target.classList.contains("charSelect") || event.target.parentElement.classList.contains("charSelect")) {
+            let charSelect;
+            event.target.classList.contains("charSelect") ? charSelect = event.target : charSelect = event.target.parentElement;
+            if(charSelect.classList.contains("selected")) charSelect.classList.remove("selected");
+            else if(charSelect.classList.contains("catDiv") && checkCatSelected() === false) {
+                charSelect.classList.add("selected")
+            }
+            else if(charSelect.classList.contains("dogDiv") && checkDogSelected() === false) {
+                charSelect.classList.add("selected")
+            }
+        }
+    }
+
+    function checkCatSelected() {
+        let catSelected = false;
+        for(let cat of catDivs) {
+            if (cat.classList.contains("selected")) catSelected = true;
+        }
+        return catSelected;
+    }
+    function checkDogSelected() {
+        let dogSelected = false;
+        for(let dog of dogDivs) {
+            if (dog.classList.contains("selected")) dogSelected = true;
+        }
+        return dogSelected;
+    }
+    function unselectCatsDogs() {
+        for (let cat of catDivs) {
+            if (cat.classList.contains("selected")) cat.classList.remove("selected");
+        }
+        for (let dog of dogDivs) {
+            if (dog.classList.contains("selected")) dog.classList.remove("selected");
+        }
+    }
+
+    const addReadyButtonEventListener = () => readyButton.addEventListener("click", startNewGame);
+    function startNewGame() {
+        const selectedChars = Array.from(document.querySelectorAll(".selected"));
+        if(selectedChars.length !== 2) {
+            return
+        }
+        unselectCatsDogs();
+        GameParameters.makePlayers(
+            selectedChars[0].lastElementChild.innerHTML,
+            selectedChars[0].firstElementChild.getAttribute("src"),
+            selectedChars[1].lastElementChild.innerHTML,
+            selectedChars[1].firstElementChild.getAttribute("src")
+        );
+        GameDisplayController.toggleSelectScreen();
+        GameDisplayController.toggleGameScreen();
+        GameDisplayController.displayPlayerInfo();
+        newRound();
+    }
+
+    function addAllEventListeners() {
         addGameGridEventListener();
         addRefreshButtonEventListener();
         addNewGameButtonEventListener();
+        addExitButtonEventListener();
+        addMenuReturnButtonEventListener();
+        addSelectScreenEventListener();
+        addReadyButtonEventListener();
+    }
+    function addGameEndEventListeners() {
+        addGameGridEventListener(); 
+        addRefreshButtonEventListener();
+        addExitButtonEventListener();
+    }
+    function removeGameEndEventListeners() {
+        removeGameGridEventListener();
+        removeRefreshButtonEventListener();
+        removeExitButtonEventListener();
     }
 
-    return {addEventListeners}
+    return {addAllEventListeners}
 })();
 
 export {Interaction}
--- script.js
+++ script.js
@@ -4,19 +4,19 @@ import { GameParameters } from "./gameParameters.js";
 import { GameStateController } from "./gameStateController.js";
 import { Interaction } from "./interaction.js";
 
-GameParameters.makePlayers("Siamese", "images/siamese.jpg", "Golden Retriever", "images/goldenRetriever.jpg");
-const players = GameParameters.getPlayers();
+// GameParameters.makePlayers("Siamese", "images/siamese.jpg", "Golden Retriever", "images/goldenRetriever.jpg");
+// const players = GameParameters.getPlayers();
 
-GameParameters.addPlayerWin(players[0]);
-GameParameters.addPlayerWin(players[0]);
-GameParameters.addPlayerWin(players[0]);
-GameParameters.addPlayerWin(players[1]);
-GameParameters.addPlayerWin(players[1]);
-GameParameters.addTie();
+// GameParameters.addPlayerWin(players[0]);
+// GameParameters.addPlayerWin(players[0]);
+// GameParameters.addPlayerWin(players[0]);
+// GameParameters.addPlayerWin(players[1]);
+// GameParameters.addPlayerWin(players[1]);
+// GameParameters.addTie();
 
-GameDisplayController.toggleSelectScreen();
-GameDisplayController.toggleGameScreen();
-GameDisplayController.displayPlayerInfo();
-GameDisplayController.displayWinsTies();
+// GameDisplayController.toggleSelectScreen();
+// GameDisplayController.toggleGameScreen();
+// GameDisplayController.displayPlayerInfo();
+// GameDisplayController.displayWinsTies();
 
-Interaction.addEventListeners();
+Interaction.addAllEventListeners();
